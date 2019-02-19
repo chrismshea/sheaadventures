156da4d4f9428ec9508715196896512dd191702e
Chris Shea <chris.shea@apisource.comm>
2019-02-19 12:06:46 -0600

    Remove social Icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
-import logo from '../img/logo.svg'
-import facebook from '../img/social/facebook.svg'
-import instagram from '../img/social/instagram.svg'
-import twitter from '../img/social/twitter.svg'
-import vimeo from '../img/social/vimeo.svg'
+//import logo from '../img/logo.svg'
+//import facebook from '../img/social/facebook.svg'
+//import instagram from '../img/social/instagram.svg'
+//import twitter from '../img/social/twitter.svg'
+//import vimeo from '../img/social/vimeo.svg'
 
 const Footer = class extends React.Component {
   render() {
@@ -14,7 +14,7 @@ const Footer = class extends React.Component {
         <div className="content has-text-centered">
           <img
             src={logo}
-            alt="Kaldi"
+            alt="SheaAdventures"
             style={{ width: '14em', height: '10em' }}
           />
         </div>
@@ -26,15 +26,8 @@ const Footer = class extends React.Component {
                     <ul className="menu-list">
                       <li><Link to="/" className="navbar-item">Home</Link></li>
                       <li><Link className="navbar-item" to="/about">About</Link></li>
-                      <li><Link className="navbar-item" to="/products">
-                        Products
-                      </Link>
-                    </li>
-                    <li>
-                    <Link className="navbar-item" to="/contact/examples">
-                      Form Examples
-                    </Link>
-                    </li>
+                      <li><Link className="navbar-item" to="/products">Products</Link></li>
+                      <li><Link className="navbar-item" to="/contact/examples">Form Examples</Link></li>
                     <li><a
                       className="navbar-item"
                       href="/admin/"
