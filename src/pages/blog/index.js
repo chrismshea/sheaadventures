e945a829c1d8375e34900dc12aeaef3f82ff8756
Chris Shea <chris.shea@apisource.comm>
2019-02-18 22:48:19 -0600

    Remove images and rename post

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -14,7 +14,7 @@ export default class BlogIndexPage extends React.Component {
             <div
                 className="full-width-image-container margin-top-0"
                 style={{
-                  backgroundImage: `url('/img/blog-index.jpg')`,
+                  backgroundImage: `url('/static/img/img_1361.jpg')`,
                 }}
               >
                 <h1
